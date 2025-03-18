dc7d29f57873b310dc85e43530e0059cc4e10e70
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-18 21:56:17 +0700

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -15,10 +15,12 @@ class Camera:
         # Set auto white balance
         self.camera.set_controls({"AwbMode": libcamera.controls.AwbModeEnum.Auto})
         # Adjust other settings for better image quality
-        self.camera.set_controls({"Brightness": 1})
-        self.camera.set_controls({"Contrast": 1})
-        self.camera.set_controls({"Saturation": 1})
+        self.camera.set_controls({"Brightness": 0.5})
+        self.camera.set_controls({"Contrast": 1.5})
+        self.camera.set_controls({"Saturation": 1.5})
         self.camera.set_controls({"Sharpness": 1.0})
+        # Adjust color gains (experiment with these values)
+        self.camera.set_controls({"ColourGains": (1.5, 1.5)})
         self.camera.start()
 
     def get_frame(self):
