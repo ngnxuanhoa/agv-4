33ea280244f1f013692e937c6f2d501e7aefb083
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-18 21:21:27 +0700

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,16 +1,18 @@
+from picamera2 import Picamera2, Preview
 import cv2
-from picamera2 import Picamera2
 
 class Camera:
     def __init__(self):
         self.camera = Picamera2()
+        # Configure camera settings
         config = self.camera.create_still_configuration(
             main={"size": (1640, 1232)},
             lores={"size": (640, 480)},
             display="lores"
         )
         self.camera.configure(config)
-        self.camera.controls.AwbMode = "Auto"
+        # Set Auto White Balance
+        self.camera.set_controls({"AwbMode": "auto"})
         self.camera.start()
 
     def get_frame(self):
