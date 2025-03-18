52bf3a93f7a8f8d2710cf35bbeb388e6b6758636
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-18 22:22:41 +0700

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -12,16 +12,12 @@ class Camera:
             display="lores"
         )
         self.camera.configure(config)
-        # Set auto white balance
-        self.camera.set_controls({"AwbMode": libcamera.controls.AwbModeEnum.Auto})
         self.camera.start()
 
     def get_frame(self):
         frame = self.camera.capture_array("main")
         # Convert to 3-channel BGR format
         frame = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_I420)
-        # Convert BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         return frame
 
 if __name__ == "__main__":
