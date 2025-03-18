80b1be6093435f1d77bb093d7bce59c9554e3679
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-18 21:07:11 +0700

    Update motor_controller.py

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -1,18 +1,17 @@
 import RPi.GPIO as GPIO
-import time
 
 class MotorController:
     def __init__(self):
-        # L298N motor driver pins
-        self.ENA = 25  # Enable motor A
-        self.IN1 = 23  # Input 1
-        self.IN2 = 24  # Input 2
-        self.ENB = 22  # Enable motor B
-        self.IN3 = 17  # Input 3
-        self.IN4 = 27  # Input 4
+        self.ENA = 22
+        self.IN1 = 17
+        self.IN2 = 27
+        self.ENB = 25
+        self.IN3 = 23
+        self.IN4 = 24
 
-        # Setup GPIO
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)  # Disable GPIO warnings
+
         GPIO.setup(self.ENA, GPIO.OUT)
         GPIO.setup(self.IN1, GPIO.OUT)
         GPIO.setup(self.IN2, GPIO.OUT)
@@ -20,59 +19,52 @@ class MotorController:
         GPIO.setup(self.IN3, GPIO.OUT)
         GPIO.setup(self.IN4, GPIO.OUT)
 
-        # Setup PWM for speed control
-        self.pwm_a = GPIO.PWM(self.ENA, 100)
-        self.pwm_b = GPIO.PWM(self.ENB, 100)
-        self.pwm_a.start(0)
-        self.pwm_b.start(0)
+        self.pwm_A = GPIO.PWM(self.ENA, 100)
+        self.pwm_B = GPIO.PWM(self.ENB, 100)
+        self.pwm_A.start(0)
+        self.pwm_B.start(0)
 
-    def forward(self, speed=50):
-        """Move forward"""
-        self.pwm_a.ChangeDutyCycle(speed)
-        self.pwm_b.ChangeDutyCycle(speed)
+    def forward(self):
         GPIO.output(self.IN1, GPIO.HIGH)
         GPIO.output(self.IN2, GPIO.LOW)
         GPIO.output(self.IN3, GPIO.HIGH)
         GPIO.output(self.IN4, GPIO.LOW)
+        self.pwm_A.ChangeDutyCycle(75)
+        self.pwm_B.ChangeDutyCycle(75)
 
-    def backward(self, speed=50):
-        """Move backward"""
-        self.pwm_a.ChangeDutyCycle(speed)
-        self.pwm_b.ChangeDutyCycle(speed)
+    def backward(self):
         GPIO.output(self.IN1, GPIO.LOW)
         GPIO.output(self.IN2, GPIO.HIGH)
         GPIO.output(self.IN3, GPIO.LOW)
         GPIO.output(self.IN4, GPIO.HIGH)
+        self.pwm_A.ChangeDutyCycle(75)
+        self.pwm_B.ChangeDutyCycle(75)
 
-    def left(self, speed=40):
-        """Turn left"""
-        self.pwm_a.ChangeDutyCycle(speed)
-        self.pwm_b.ChangeDutyCycle(speed)
+    def left(self):
         GPIO.output(self.IN1, GPIO.LOW)
         GPIO.output(self.IN2, GPIO.HIGH)
         GPIO.output(self.IN3, GPIO.HIGH)
         GPIO.output(self.IN4, GPIO.LOW)
+        self.pwm_A.ChangeDutyCycle(75)
+        self.pwm_B.ChangeDutyCycle(75)
 
-    def right(self, speed=40):
-        """Turn right"""
-        self.pwm_a.ChangeDutyCycle(speed)
-        self.pwm_b.ChangeDutyCycle(speed)
+    def right(self):
         GPIO.output(self.IN1, GPIO.HIGH)
         GPIO.output(self.IN2, GPIO.LOW)
         GPIO.output(self.IN3, GPIO.LOW)
         GPIO.output(self.IN4, GPIO.HIGH)
+        self.pwm_A.ChangeDutyCycle(75)
+        self.pwm_B.ChangeDutyCycle(75)
 
     def stop(self):
-        """Stop motors"""
-        self.pwm_a.ChangeDutyCycle(0)
-        self.pwm_b.ChangeDutyCycle(0)
         GPIO.output(self.IN1, GPIO.LOW)
         GPIO.output(self.IN2, GPIO.LOW)
         GPIO.output(self.IN3, GPIO.LOW)
         GPIO.output(self.IN4, GPIO.LOW)
+        self.pwm_A.ChangeDutyCycle(0)
+        self.pwm_B.ChangeDutyCycle(0)
 
     def cleanup(self):
-        """Cleanup GPIO"""
-        self.pwm_a.stop()
-        self.pwm_b.stop()
+        self.pwm_A.stop()
+        self.pwm_B.stop()
         GPIO.cleanup()
